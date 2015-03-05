81381f192906cdf714438f0e2a9c94efd6321706
martinsunder <martinsunder@hotmail.com>
2015-03-05 10:50:40 +0000

    Initial Import

---
+++ HelloGit/src/net/martin/HelloGitWorld.java
@@ -0,0 +1,10 @@
+package net.martin;
+
+public class HelloGitWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
